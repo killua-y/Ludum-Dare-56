d4b2508a853c18fb4d766e3b4b287b1bc5897011
Xinghan Yan <76619533+killua-y@users.noreply.github.com>
2024-10-07 15:09:46 -0400

    Gold release

--- Assets/scripts/Menu/MainMenu.cs
+++ Assets/scripts/Menu/MainMenu.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject ReditPage;
+    public GameObject CreditButton;
+    public Transform position1;
+    public Transform position2;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,11 +31,17 @@ public class MainMenu : MonoBehaviour
     {
         if (ReditPage.activeSelf)
         {
+            CreditButton.transform.position = position1.transform.position;
             ReditPage.SetActive(false);
+            CreditButton.GetComponent<Button>().interactable = false;
+            CreditButton.GetComponent<Button>().interactable = true;
         }
         else
         {
+            CreditButton.transform.position = position2.transform.position;
             ReditPage.SetActive(true);
+            CreditButton.GetComponent<Button>().interactable = false;
+            CreditButton.GetComponent<Button>().interactable = true;
         }
     }
 }
